7c09c0b508bc62a33eb47efd37f423a68997b158
Eivind Schulstad <eivind.schulstad@pragroup.no>
2022-04-13 20:42:26 +0200

    Added endpoint to download a picture by id

--- Socializer.Service/API/Controllers/ImagesController.cs
+++ Socializer.Service/API/Controllers/ImagesController.cs
@@ -9,25 +9,39 @@ namespace API.Controllers
     {
         private readonly ILogger<UsersController> _logger;
         private readonly IWebGalleryFileDownloader _webGalleryFileDownloader;
+        private readonly IWebGalleryService _webGalleryService;
 
-        public ImagesController(ILogger<UsersController> logger, IWebGalleryFileDownloader webGalleryFileDownloader)
+        public ImagesController(ILogger<UsersController> logger, IWebGalleryFileDownloader webGalleryFileDownloader, IWebGalleryService webGalleryService)
         {
             _logger = logger;
             _webGalleryFileDownloader = webGalleryFileDownloader;
+            _webGalleryService = webGalleryService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> WebGalleryPictureById(string id)
+        {
+            var pic = await _webGalleryService.GetPictureById(id);
+            var appPathBytes = System.Text.Encoding.UTF8.GetBytes(pic.PictureAppPath);
+            string appPathBase64 = Convert.ToBase64String(appPathBytes);
+            
+            var fileBytes = await _webGalleryFileDownloader.DownloadImageFromFileServer(appPathBase64, "testuser");
+
+            return new FileContentResult(fileBytes, "image/jpeg");
+        }
+
+        [HttpGet("webgallery/{appPathBase64}")]
+        public async Task<IActionResult> WebGalleryPictureByAppPath(string appPathBase64)
+        {
+            var fileBytes = await _webGalleryFileDownloader.DownloadImageFromFileServer(appPathBase64, "testuser");
+
+            return new FileContentResult(fileBytes, "image/jpeg");
         }
 
         [HttpGet("profiles/{username}")]
         public IActionResult ProfilePicture(string username)
         {
             var fileBytes = new ProfilePictureService().GetProfilePictureFromDisk(username);
-
-            return new FileContentResult(fileBytes, "image/jpeg");
-        }
-
-        [HttpGet("webgallery/{appPathBase64}")]
-        public async Task<IActionResult> WebGalleryPicture(string appPathBase64)
-        {
-            var fileBytes = await _webGalleryFileDownloader.DownloadImageFromFileServer(appPathBase64, "testuser");
 
             return new FileContentResult(fileBytes, "image/jpeg");
         }
--- Socializer.Service/DataAccess/WebGalleryService.cs
+++ Socializer.Service/DataAccess/WebGalleryService.cs
@@ -6,6 +6,7 @@ namespace Infrastructure
     public interface IWebGalleryService
     {
         Task<PostPicture> GetRandomPicture();
+        Task<PostPicture> GetPictureById(string id);
     }
 
     public class WebGalleryService : IWebGalleryService
@@ -19,13 +20,32 @@ namespace Infrastructure
             _webGalleryUser = webGalleryApiUser;
         }
 
+        public async Task<PostPicture> GetPictureById(string id)
+        {
+            HttpClient client = CreateApiClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"pictures/sha/{id}");
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                var postPicture = JsonConvert.DeserializeObject<PictureDTO>(result);
+
+                return new PostPicture
+                {
+                    PictureId = postPicture.Id,
+                    PictureAppPath = postPicture.AppPath,
+                    Tags = postPicture.Tags,
+                };
+            }
+            else
+            {
+                throw new Exception($"The Web Gallery API returned a {response.StatusCode} status code.");
+            }
+        }
+
         public async Task<PostPicture> GetRandomPicture()
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(_webGalleryApiEndpoint);
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-            client.DefaultRequestHeaders.Add("Gallery-User", _webGalleryUser);
-
+            HttpClient client = CreateApiClient();
             var request = new HttpRequestMessage(HttpMethod.Get, $"pictures/random");
             var response = await client.SendAsync(request);
             if (response.IsSuccessStatusCode)
@@ -44,6 +64,16 @@ namespace Infrastructure
             {
                 throw new Exception($"The Web Gallery API returned a {response.StatusCode} status code.");
             }
+        }
+
+        private HttpClient CreateApiClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_webGalleryApiEndpoint);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Gallery-User", _webGalleryUser);
+
+            return client;
         }
     }
 
